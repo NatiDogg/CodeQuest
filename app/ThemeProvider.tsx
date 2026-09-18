40454ab36dc96536f74f77cfd916a2b29b1e265d
natiDogg <natnael.wondmu@gmail.com>
2026-09-18 12:36:50 -0700

    saved the user in the db

--- app/ThemeProvider.tsx
+++ app/ThemeProvider.tsx
@@ -2,15 +2,21 @@
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { useUser } from "@clerk/nextjs"
-
+import axios from 'axios'
 const ThemeProvider = ({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) => {
    const {user} = useUser()
 
-    const createNewUser = ()=>{
-      
+    React.useEffect(()=>{
+         user && createNewUser()
+    },[user])
+    const createNewUser = async()=>{
+        const response = await axios.post('/api/user', {})
+
+        console.log(response.data)
+        
     }
   return (
     <NextThemesProvider {...props}>
--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,9 +1,35 @@
+import { prisma } from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async(req:NextRequest)=>{
    
      const user = await currentUser()
 
+     if(!user){
+          return NextResponse.json({error: 'Unauthorized'}, {status: 401})
+     }
+
+     const email = user.primaryEmailAddress?.emailAddress;
+  
+       if (!email) {
+           return NextResponse.json(
+             { error: "User missing primary email" }, 
+                 { status: 400 }
+             );
+       }
+
+       const name = user.fullName ?? "";
+
+       const dbUser = await prisma.user.upsert({
+          where: { email },
+           update: {}, 
+           create: {
+            email,
+            name,
+           },
+       });
+     
+      return NextResponse.json({user:dbUser}, {status: 200})
      
 }
