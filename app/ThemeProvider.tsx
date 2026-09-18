ae1d8e03928d42570258f0a3307c5f673e8ed6e8
natiDogg <natnael.wondmu@gmail.com>
2026-09-18 13:21:36 -0700

    created user context to make the user data avaliable to every component

--- app/ThemeProvider.tsx
+++ app/ThemeProvider.tsx
@@ -3,24 +3,28 @@ import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { useUser } from "@clerk/nextjs"
 import axios from 'axios'
+import { UserContext } from "@/context/userContext"
 const ThemeProvider = ({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) => {
    const {user} = useUser()
+   const [userDetail, setUserDetail] = React.useState()
 
     React.useEffect(()=>{
          user && createNewUser()
     },[user])
     const createNewUser = async()=>{
         const response = await axios.post('/api/user', {})
-
-        console.log(response.data)
+        setUserDetail(response.data.user)
         
     }
   return (
     <NextThemesProvider {...props}>
-        {children}
+        <UserContext.Provider value={{userDetail, setUserDetail}}>
+             {children}
+        </UserContext.Provider>
+        
         </NextThemesProvider>
   )
 }
