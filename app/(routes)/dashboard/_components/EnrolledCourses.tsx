d354ba907cb28d65c4d8bbfd06ff24892b1e2b52
natiDogg <natnael.wondmu@gmail.com>
2026-09-23 12:50:44 -0700

    done with my explore more component

--- app/(routes)/dashboard/_components/EnrolledCourses.tsx
+++ app/(routes)/dashboard/_components/EnrolledCourses.tsx
@@ -8,7 +8,7 @@ const EnrolledCourses = () => {
 
   return (
     <div className="w-full flex flex-col gap-4">
-      <h2 className="text-3xl md:text-4xl font-bold font-game text-white"> Your Enrolled Courses</h2>
+      <h2 className="text-2xl md:text-3xl font-bold font-game text-white"> Your Enrolled Courses</h2>
       
       {enrolledCourses.length > 0 ? (
         <div className="w-full">display enrolled courses</div>
@@ -16,7 +16,7 @@ const EnrolledCourses = () => {
         <div className="w-full flex flex-col gap-4 items-center justify-center bg-zinc-800 rounded-2xl p-4">
           <Image src={'/books.png'} alt="books" width={100} height={100} />
           <h2 className="text-white text-2xl font-game">You dont have any course enrolled</h2>
-          <Button variant={'pixel'}>Browse All Courses</Button>
+          <Button className={'font-game cursor-pointer'} variant={'pixel'}>Browse All Courses</Button>
         </div>
       )}
     </div>
